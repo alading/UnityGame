0eb58bf79ddfc23ac3938357a6c7dfac329b5d0d
Weimin Ding <dingweimin@tcl.com>
2017-03-15 17:45:02 -0700

    add rolling ball

---
+++ RollingBall/RollingBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+	public Text countText;
+
+	Vector3 zeroAc;
+	Vector3 curAc;
+	float sensH = 5f;
+	float sensV = 5f;
+	float smooth = 0.5f;
+	float GetAxisH  = 0f;
+	float GetAxisV = 0f;
+
+	void ResetAxes(){
+		zeroAc = Input.acceleration;
+		curAc = Vector3.zero;
+	}
+
+
+	// start the game
+	void Start() {
+
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		this.GetComponent<ParticleSystem>().Stop();
+		ResetAxes ();
+	
+
+	}
+
+
+	// physic update
+
+	private void Update () {
+		
+	}
+
+	void FixedUpdate() {
+
+		if (SystemInfo.deviceType == DeviceType.Handheld) {
+			Debug.Log ("mobile");
+			curAc = Vector3.Lerp (curAc, Input.acceleration - zeroAc, Time.deltaTime / smooth);
+			GetAxisV = Mathf.Clamp (curAc.y * sensV, -1, 1);
+			GetAxisH = Mathf.Clamp (curAc.x * sensH, -1, 1);
+			rb.AddForce (new Vector3 (GetAxisH, 0.0f, GetAxisV) * speed);
+		} else {
+
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			rb.AddForce (movement * speed);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Score: " + count.ToString ();
+		if (count > 14)
+		{
+			countText.text = "You win";
+			this.GetComponent<ParticleSystem>().Play();
+		}
+	}
+}
---
+++ RollingBall/RollingBall/Assets/Scripts/Share.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Share : MonoBehaviour {
+
+	string subject = "Rolling Ball";
+	string body = "PLAY THIS GAME NOW";
+
+	// Use this for initialization
+	public void ShareApp() {
+		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+		currentActivity.Call ("shareText", subject, body);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
